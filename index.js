8724f59b2d537aafdd5e073fd5d228f37961fc87
lamaisrama <lamaisrama@gmail.com>
2020-10-11 23:55:48 +0900

    Express Server 생성. package.json에 npm script 추가

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi');
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+function handleListening () {
+    console.log(`Listening on : http://127.0.0.1:${PORT} 💚`);
+}
+
+app.listen(PORT, handleListening);
+
+// respond with 'hello world ' when a GET req is made to the homepage
+app.get('/', function(req, res){
+    res.send('hello world')
+})
