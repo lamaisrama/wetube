389ca49f979197b6ba160d98493329a3216dc404
lamaisrama <lamaisrama@gmail.com>
2020-10-21 22:46:36 +0900

    #2.6 Express core - middleware

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const app = express();
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on : http://127.0.0.1:${PORT} 💚`);
+
 const handleHome = (req, res) => res.send('Hello World!')
 
+const handleProfile = (req, res) => res.send("This is my profile!")
+
+const betweenHome = (req, res, next) => {
+    console.log("I'm between!");
+    next();
+}
+//하나의 라우터에만 middleware 적용
+//app.get('/', betweenHome,handleHome);
+//
+app.use(betweenHome);
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+
 app.listen(PORT, handleListening);
-
-app.get('/', handleHome)
-
-
