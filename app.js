cd823eeca01e01e9c2315a0b67207798585f844a
lamaisrama <lamaisrama@gmail.com>
2020-11-08 00:33:28 +0900

    #2.10 mvc2, routers.js를 통해 전역에서 사용할 라우터객체 생성, routers.js 이용해 router 설정하기

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -19,10 +20,10 @@ app.use(logger("dev")); //morgan에는 여러 모드 있음 - tiny, combined, co
 
 
 //globalRouter
-app.use("/", globalRouter);
+app.use(routes.home, globalRouter);
 //user에 접속하면 userRouter 전체를 쓰겠다는 뜻
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 //누가 app을 부르면 app object를 주겠다는 뜻
 export default app;
---
+++ routes.js
@@ -0,0 +1,42 @@
+//divide and conquer.
+//하나의 source를 사용하고 싶다. 어디서든 사용가능한 URL을 모아놓겠다.
+
+// Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// User
+const USERS = "/users";
+const USER_DETAIL ="/:id";  // express가 : 를 사용하면 변하는 값이 들어올 것을 인지함
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+//Videos
+
+const VIDEOS = "/videos";
+const UPLOAD = "/upload"
+const VIDEO_DETAIL ="/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+    home : HOME,
+    join : JOIN,
+    login : LOGIN,
+    logout : LOGOUT,
+    search : SEARCH,
+    users : USERS,
+    userDetail : USER_DETAIL,
+    editProfile : EDIT_PROFILE,
+    changePassword : CHANGE_PASSWORD,
+    videos : VIDEOS,
+    upload : UPLOAD,
+    videoDetail : VIDEO_DETAIL,
+    editVideo : EDIT_VIDEO,
+    deleteVideo : DELETE_VIDEO
+}
+
+export default routes;
