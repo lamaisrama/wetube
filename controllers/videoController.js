9a7c446c50a4cef08191b2c9ac3b00b6df8d238c
lamaisrama <lamaisrama@gmail.com>
2020-12-08 03:52:00 +0900

    #2.18 GET method를 통해 input value 가지고 와서 controller에 variable 넘기기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,16 @@
 export const home = (req, res) => res.render("home", {pageTitle : "HOME"});
 //render 함수가 views 폴더에서 파일명이 home이고 확장자가 pug인 탬플릿 파일 찾은 후 보여줄 것
-export const search = (req, res) => res.render("search", {pageTitle : "Search"}); 
+export const search = (req, res) => {
 
+    //const searchingBy = req.query.term // ES6 이전의 코딩 방법
+    const{
+        query : { term: searchingBy } 
+    } = req;
+
+    res.render("search", {pageTitle : "Search", searchingBy : searchingBy}); 
+
+
+}
 //export const videos = (req, res) => res.send("Videos");
 export const upload = (req, res) => res.send("Upload", {pageTitle : "Upload"});
 export const videoDetail = (req, res) => res.send("Video Detail", {pageTitle : "Video Detail"} );
--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,5 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";   
     res.locals.routes = routes;
-    next(); // middleware, next req로 넘겨주어야 함.
+    next(); // middleware, next req로 넘겨주어야 함. 
 }
