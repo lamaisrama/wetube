ea38de66ef7cb14903e9f86dc15897539e730125
lamaisrama <lamaisrama@gmail.com>
2020-12-17 23:40:13 +0900

    #3.2 Make DB model

--- db.js
+++ db.js
@@ -11,5 +11,6 @@ const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+
 db.once("open", handleOpen);
 db.on("error", handleError);
---
+++ models/Video.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const VideoSchema  = new mongoose.Schema({
+    fileUrl: {
+        type : String,
+        required : 'File URL is required' //만약 없을 시 error message 넣기
+    },
+
+    title : {
+        type : String,
+        required : "Title is required"
+    },
+    description : {
+        type : String
+    },
+    views : {
+        type : Number,
+        default : 0
+    },
+    createAt : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+const model = mongoose.model("Video", VideoSchema);
+
+export default model;
