b7640b99db9e58d0eceaa71727e16353b778ed5d
lamaisrama <lamaisrama@gmail.com>
2021-01-06 00:18:06 +0900

    #3.12 Searching Videos - mongoose + regExp 사용

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,17 +10,18 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "HOME", videos: [] });
   }
 }; // render 함수가 views 폴더에서 파일명이 home이고 확장자가 pug인 탬플릿 파일 찾은 후 보여줄 것
-export const search = (req, res) => {
+export const search = async(req, res) => {
   // const searchingBy = req.query.term // ES6 이전의 코딩 방법
   const {
     query: { term: searchingBy },
   } = req;
-
-  res.render("search", {
-    pageTitle: "Search",
-    searchingBy,
-    videos,
-  });
+  let videos =[];
+  try{
+    videos = await Video.find({title: {$regex: searchingBy, $options: "i"}})
+  }catch(error){
+    console.log(error)
+  }
+  res.render("search", {pageTitle: "Search", searchingBy, videos});
 };
 // export const videos = (req, res) => res.send("Videos");
 export const getUpload = (req, res) =>
